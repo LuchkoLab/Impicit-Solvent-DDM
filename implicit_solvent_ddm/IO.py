faa86e332a4f784ab9d90d7bb42d7c9993e112bf
Steven Ayoub <steven.ayoub.362@my.csun.edu>
2022-01-03 16:38:05 -0800

    module to handle all data exports

---
+++ implicit_solvent_ddm/IO.py
@@ -0,0 +1,31 @@
+import os 
+
+def export_outputs(toil_job, output_dir, **kwargs):
+    for arg in kwargs:
+        output_filename = os.path.basename(kwargs[arg])
+        output_file = toil_job.fileStore.writeGlobalFile(output_filename)
+        toil_job.fileStore.exportFile(output_file,"file://" + os.path.abspath(os.path.join(output_dir, output_filename)))
+        if arg == 'restart':
+            coordinate_restart = output_file
+    return coordinate_restart
+    '''
+    mdout_filename = "mdout"
+    mdinfo_filename = "mdinfo"
+    restrt_filename = "restrt"
+    mdcrd_filename = "mdcrd"
+
+
+    mdout_file = toil_job.fileStore.writeGlobalFile(mdout_filename)
+    mdinfo_file = toil_job.fileStore.writeGlobalFile(mdinfo_filename)
+    restrt_file = toil_job.fileStore.writeGlobalFile(restrt_filename)
+    #mdcrd_file = toil_job.fileStore.writeGlobalFile(mdcrd_filename)
+    
+    #export all files 
+    toil_job.fileStore.exportFile(mdin,"file://" + os.path.abspath(os.path.join(output_dir, os.path.basename(mdin))))
+    toil_job.fileStore.exportFile(mdout_file, "file://" + os.path.abspath(os.path.join(output_dir, "mdout")))
+    toil_job.fileStore.exportFile(mdinfo_file, "file://" + os.path.abspath(os.path.join(output_dir, "mdinfo")))
+    toil_job.fileStore.exportFile(restrt_file, "file://" + os.path.abspath(os.path.join(output_dir,"restrt")))
+    #toil_job.fileStore.exportFile(mdcrd_file, "file://" + os.path.abspath(os.path.join(output_dir,"mdcrd")))
+    #job.fileStore.exportFile(restraint, "file://" + os.path.abspath(os.path.join(output_dir,"restraint.RST")))
+    toil_job.fileStore.exportFile(solute, "file://" + os.path.abspath(os.path.join(output_dir, str(os.path.basename(solute)))))
+    '''
