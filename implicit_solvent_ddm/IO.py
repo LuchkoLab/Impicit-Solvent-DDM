4acb4dce73817585296f768813b2ad1b1d0836cf
Steven Ayoub <steven.ayoub.362@my.csun.edu>
2022-01-20 12:32:05 -0800

    IO.py module returns two trajectory file types
    returns rst7 and nc trajectories for later post-processing

--- implicit_solvent_ddm/IO.py
+++ implicit_solvent_ddm/IO.py
@@ -3,16 +3,19 @@ import re
 def export_outputs(toil_job, output_dir, files_to_ignore):
     toil_job.log(f"Exporting files to {output_dir} ")
     restart_files = []
+    traj_files = [] 
     for root, dirs, files in os.walk(".", topdown=False):
         for name in files:
             if name in files_to_ignore:
                 continue
             output_file = toil_job.fileStore.writeGlobalFile(name)
             toil_job.fileStore.exportFile(output_file,"file://" + os.path.abspath(os.path.join(output_dir, name)))
-            if re.match(r".*.rst.*", name):
+            if re.match(r".*.rst7.*", name):
                 restart_files.append(str(output_file))
-                
-    return restart_files
+            if re.match(r".*.nc.*", name):
+                traj_files.append(str(output_file))
+    toil_job.log(f"the current trajectory files {traj_files}")
+    return restart_files, traj_files
     # if runtype == 'min':
     #     for arg in kwargs:
     #         output_filename = os.path.basename(kwargs[arg])
