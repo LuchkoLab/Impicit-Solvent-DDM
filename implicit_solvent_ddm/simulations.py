9b1fd27e73a5e905286f7ba733923e03cf1214b2
Steven Ayoub <steven.ayoub.362@my.csun.edu>
2022-01-26 15:40:33 -0800

    importes flat bottom restraints
    refactoring needed!

--- implicit_solvent_ddm/simulations.py
+++ implicit_solvent_ddm/simulations.py
@@ -1,19 +1,26 @@
 
 
+from ast import arguments
 from genericpath import exists
+from platform import architecture
 import subprocess
 import string
 import os, os.path 
-
+import re
+import sys
+import yaml
+import parmed as pmd 
 from string import Template 
 #local imports 
 from implicit_solvent_ddm.restraints import write_empty_restraint_file
 from implicit_solvent_ddm.restraints import write_restraint_forces
 from implicit_solvent_ddm.alchemical import turn_off_charges 
 from implicit_solvent_ddm.alchemical import alter_topology_file
+from implicit_solvent_ddm.remd import copy 
+from implicit_solvent_ddm.remd import write_empty_restraint
+from implicit_solvent_ddm.IO import export_outputs
 
-
-def run_md(job, solute_file, solute_filename, solute_rst, solute_rst_filename, output_dir, argSet, message, work_dir= None, ligand_mask = None, receptor_mask = None, conformational_restraint = None, orientational_restraint = None, solvent_turned_off=False, charge_off = False, exculsions=False):
+def run_md(job, solute_file, solute_filename, solute_rst, solute_rst_filename, output_dir, argSet, message, COM=False, input_mdin=None, work_dir= None, ligand_mask = None, receptor_mask = None, conformational_restraint = None, orientational_restraint = None, solvent_turned_off=False, charge_off = False, exculsions=False):
     """
     Locally run AMBER library engines for molecular dynamics
 
@@ -49,49 +56,58 @@ def run_md(job, solute_file, solute_filename, solute_rst, solute_rst_filename, o
 
     """
     job.log("this is the current job running {}".format(message))
-
+    job.log(f"input mdin {input_mdin}")
     tempDir = job.fileStore.getLocalTempDir()
-    
+    if type(solute_rst_filename) == list:
+        job.log(f'the list solute complex is {solute_rst_filename}')
+        solute_rst_filename = os.path.basename(solute_rst_filename[0])
 
     if charge_off: 
         temp_solute_filename = job.fileStore.readGlobalFile(solute_file,  userPath=os.path.join(tempDir, solute_filename))
-        rst = job.fileStore.readGlobalFile(solute_rst, userPath=os.path.join(tempDir, solute_rst_filename))
+        rst = job.fileStore.readGlobalFile(solute_rst[0], userPath=os.path.join(tempDir, solute_rst_filename))
         # import ligand parmtop into temporary directory 
         #charge_off_solute_filename = job.fileStore.importFile("file://" + turn_off_charges(temp_solute_filename, rst, ligand_mask))        
         altered_solute_filename = job.fileStore.importFile("file://" + alter_topology_file(temp_solute_filename, rst, ligand_mask, receptor_mask, charge_off, exculsions))
         #solute = job.fileStore.readGlobalFile(charge_off_solute_filename,  userPath=os.path.join(tempDir, os.path.basename(charge_off_solute_filename)))
         solute = job.fileStore.readGlobalFile(altered_solute_filename, userPath=os.path.join(tempDir, os.path.basename(altered_solute_filename)))
-
     else:
         solute = job.fileStore.readGlobalFile(solute_file,  userPath=os.path.join(tempDir, solute_filename))
-        rst = job.fileStore.readGlobalFile(solute_rst, userPath=os.path.join(tempDir, solute_rst_filename))
+        rst = job.fileStore.readGlobalFile(solute_rst[0], userPath=os.path.join(tempDir, solute_rst_filename))
 
-    if argSet["parameters"]["mdin"]:
-        mdin_filename  = job.fileStore.importFile(os.path.join("file://" + work_dir, argSet["parameters"]["mdin"]))
-        mdin = job.fileStore.readGlobalFile(mdin_filename, userPath=os.path.join(tempDir, os.path.basename(argSet["parameters"]["mdin"])))
+    if input_mdin:
+        mdin_filename  = job.fileStore.importFile(os.path.join("file://" + work_dir, input_mdin))
+        mdin = job.fileStore.readGlobalFile(mdin_filename, userPath=os.path.join(tempDir, os.path.basename(input_mdin)))
         
     else:
-        mdin_filename = job.fileStore.importFile("file://" + make_mdin_file(conformational_restraint, solvent_turned_off))
+        mdin_filename = job.fileStore.importFile("file://" + make_mdin_file(argSet, conformational_restraint, solvent_turned_off))
         mdin = job.fileStore.readGlobalFile(mdin_filename, userPath=os.path.join(tempDir, os.path.basename(mdin_filename)))
+    
+    flat_bottom = argSet["parameters"]["flat_bottom_restraints"][0]
+    
+    if COM:
+        import_restraint = job.fileStore.importFile("file://" + copy(flat_bottom))
         
-    if conformational_restraint == None:
-        restraint_file = job.fileStore.importFile("file://" + write_empty_restraint_file())
+    elif conformational_restraint == None:
+        import_restraint =  job.fileStore.importFile("file://" + write_empty_restraint(flat_bottom))
+            
+        # restraint_file = job.fileStore.importFile("file://" + write_empty_restraint_file())
 
-        restraint_basename = os.path.basename(restraint_file)
-        restraint = job.fileStore.readGlobalFile( restraint_file, userPath=os.path.join(tempDir, restraint_basename))
+        # restraint_basename = os.path.basename(restraint_file)
+        # restraint = job.fileStore.readGlobalFile( restraint_file, userPath=os.path.join(tempDir, restraint_basename))
              
         if not os.path.exists(output_dir + '/0'):
             output_dir = os.path.join(output_dir + '/0')
             os.makedirs(output_dir)
 
     
-    if conformational_restraint != None:
-        restraint_file = job.fileStore.importFile("file://" + write_restraint_forces(solute_filename, work_dir, conformational_restraint, orientational_restraint))
-
+    elif conformational_restraint != None:
+        
+        job.log("CONFORMATIONAL TURN ON")
+        restraint_file = job.fileStore.importFile("file://" + write_restraint_forces(solute_filename, work_dir, conformational_restraint, orientational_restraint)) 
         restraint_basename = os.path.basename(restraint_file)
         job.log('restraint_freeze_file : ' + str(restraint_file))
         restraint = job.fileStore.readGlobalFile(restraint_file, userPath=os.path.join(tempDir, restraint_basename))
-
+        
         if orientational_restraint != None:
             if not os.path.exists(output_dir + '/' + str(conformational_restraint) + '_' + str(orientational_restraint)):
                 output_dir = os.path.join(output_dir + '/'+ str(conformational_restraint) + '_' + str(orientational_restraint))
@@ -101,48 +117,104 @@ def run_md(job, solute_file, solute_filename, solute_rst, solute_rst_filename, o
             if not os.path.exists(output_dir + '/' + str(conformational_restraint)):
                 output_dir = os.path.join(output_dir + '/'+ str(conformational_restraint))
                 os.makedirs(output_dir)
+                job.log(f"no orientaional restraints make output dir {output_dir}")
         
-    if argSet["parameters"]["mpi"]:
-        exe = argSet["parameters"]["executable"]
-        np = str(argSet["parameters"]["mpi"])
-        subprocess.check_call(["mpirun", "-np", np, exe, "-O", "-i", mdin, "-p", solute, "-c", rst])
-    else: 
-        exe = argSet["parameters"]["executable"]
-        subprocess.check_call([exe, "-O", "-i", mdin, "-p", solute, "-c", rst])
-
-    mdout_filename = "mdout"
-    mdinfo_filename = "mdinfo"
-    restrt_filename = "restrt"
-    mdcrd_filename = "mdcrd"
-
-
-    mdout_file = job.fileStore.writeGlobalFile(mdout_filename)
-    mdinfo_file = job.fileStore.writeGlobalFile(mdinfo_filename)
-    restrt_file = job.fileStore.writeGlobalFile(restrt_filename)
-    mdcrd_file = job.fileStore.writeGlobalFile(mdcrd_filename)
-    if conformational_restraint != None:
-        job.log("running current conformational restraint value : " +str(conformational_restraint))
-        job.log("conformational restraint mdout file: " + str(mdout_file))
+        job.fileStore.exportFile(restraint, "file://" + os.path.abspath(os.path.join(output_dir,"restraint.RST")))
     
-    #export all files 
-    job.fileStore.exportFile(mdin,"file://" + os.path.abspath(os.path.join(output_dir, "mdin")))
-    job.fileStore.exportFile(mdout_file, "file://" + os.path.abspath(os.path.join(output_dir, "mdout")))
-    job.fileStore.exportFile(mdinfo_file, "file://" + os.path.abspath(os.path.join(output_dir, "mdinfo")))
-    job.fileStore.exportFile(restrt_file, "file://" + os.path.abspath(os.path.join(output_dir,"restrt")))
-    job.fileStore.exportFile(mdcrd_file, "file://" + os.path.abspath(os.path.join(output_dir,"mdcrd")))
-    job.fileStore.exportFile(restraint, "file://" + os.path.abspath(os.path.join(output_dir,"restraint.RST")))
-    job.fileStore.exportFile(solute, "file://" + os.path.abspath(os.path.join(output_dir, str(os.path.basename(solute)))))
+    files_in_current_directory = os.listdir(tempDir)  
+    job.log(f"files before simulations {files_in_current_directory}")
+    
+    #name restart and trajectory files 
+    solu = re.sub(r"\..*","",solute_filename)
+    restart_filename = f"{message}_{solu}.rst7"
+    trajectory_filename = f"{solu}.nc"
+    
+    run_args = {'solute': solute,
+                'coordinate': rst,
+                "restart_filename": restart_filename,
+                "trajectory_filename": trajectory_filename,
+                "mdin": mdin
+                }
+    submit_job(job, argSet, run_args)
+    
+    # if argSet["parameters"]["mpi"]:
+    #     exe = argSet["parameters"]["executable"].split()
+    #     exe.append()
+    #     np = str(argSet["parameters"]["mpi"])
+    #     output = subprocess.run(["srun", "-n", np, exe, "-O", "-i", mdin, 
+    #                              "-p", solute, "-c", rst, 
+    #                              "-r",restart_filename , "-", trajectory_filename], 
+    #                             capture_output=True)
+    # else: 
+    #     exe = argSet["parameters"]["executable"]
+    #     output = subprocess.run([exe, "-O", "-i", mdin, 
+    #                              "-p", solute, "-c", rst,
+    #                              "-r",restart_filename , "-x", trajectory_filename],
+    #                             capture_output=True)
+    restrt_file,mdcrd_file = export_outputs(job, output_dir, files_in_current_directory)
 
-    if message == 'complex':
-         
-        return restrt_file, mdcrd_file
+    
+    # mdout_filename = "mdout"
+    # mdinfo_filename = "mdinfo"
+    # restrt_filename = "restrt"
+    # mdcrd_filename = "mdcrd"
+
+
+    # mdout_file = job.fileStore.writeGlobalFile(mdout_filename)
+    # mdinfo_file = job.fileStore.writeGlobalFile(mdinfo_filename)
+    # restrt_file = job.fileStore.writeGlobalFile(restrt_filename)
+    # mdcrd_file = job.fileStore.writeGlobalFile(mdcrd_filename)
+    
+    # if conformational_restraint != None:
+    #     job.log("running current conformational restraint value : " +str(conformational_restraint))
+    #     job.log("conformational restraint mdout file: " + str(mdout_file))
+    
+    job.fileStore.exportFile(solute, "file://" + os.path.abspath(os.path.join(output_dir, str(os.path.basename(solute)))))
+    return (restrt_file,mdcrd_file)
+    # #export all files 
+    # job.fileStore.exportFile(mdin,"file://" + os.path.abspath(os.path.join(output_dir, "mdin")))
+    # job.fileStore.exportFile(mdout_file, "file://" + os.path.abspath(os.path.join(output_dir, "mdout")))
+    # job.fileStore.exportFile(mdinfo_file, "file://" + os.path.abspath(os.path.join(output_dir, "mdinfo")))
+    # job.fileStore.exportFile(restrt_file, "file://" + os.path.abspath(os.path.join(output_dir,"restrt")))
+    # job.fileStore.exportFile(mdcrd_file, "file://" + os.path.abspath(os.path.join(output_dir,"mdcrd")))
+    
+
 
 def initilized_jobs(job, work_dir):
     #job.log("Hello world, I have a message: {}".format(message))
     job.log(f'initialized job, the current working directory is {work_dir}')
 
 
-def make_mdin_file(turn_on_conformational_rest, turn_off_solvent):
+def submit_job(job, arguments, run_arguments):
+    
+    system = pmd.load_file(run_arguments["solute"], run_arguments["coordinate"])
+     
+    argument_list = [
+        "-O",
+        "-i", run_arguments["mdin"],
+        "-p", run_arguments["solute"], 
+        "-c", run_arguments["coordinate"], 
+        "-r",run_arguments["restart_filename"] , 
+        "-x", run_arguments["trajectory_filename"]
+    ]
+    exe = arguments["parameters"]["executable"].split()
+    
+    if len(system.residues) > 1:
+        #exe.append("-O")
+        exe += argument_list
+        #np = str(arguments["parameters"]["mpi"])
+        output = subprocess.run(exe,capture_output=True)
+    else:
+        if bool(re.search(r"\.", arguments["parameters"]["executable"])): 
+            serial_exe = [re.sub(r"\..*","",exe[-1])]
+        else:
+            serial_exe = exe
+        #exe.append("-O")
+        serial_exe += argument_list
+        output = subprocess.run(serial_exe,capture_output=True)
+    job.log(f"the output error {output}")
+    
+def make_mdin_file(arguments, turn_on_conformational_rest, turn_off_solvent):
     """ Creates an molecular dynamics input file
 
     Function will fill a template and write an MD input file
@@ -157,58 +229,67 @@ def make_mdin_file(turn_on_conformational_rest, turn_off_solvent):
     mdin: str
         Absolute path where the MD input file was created.
     """
-
+    with open(arguments["parameters"]["mdin_intermidate_config"]) as mdin_values:
+        user_inputs = yaml.safe_load(mdin_values)
+    
     mdin_path = os.path.abspath(os.path.dirname(
                 os.path.realpath(__file__)) + "/templates/mdgb.mdin")
+    
     with open(mdin_path) as t:
         template = Template(t.read())
     
     if turn_on_conformational_rest==None:
         final_template = template.substitute(
-            nstlim=1000,
+            nstlim=user_inputs["nstlim"],
             ntx=1,
             irest=0,
-            dt=0.001,
-            igb = 2,
-            saltcon = 0.3,
-            gbsa=0,
-            temp0=298,
-            ntpr=10,
-            ntwx=10,
-            cut=999,
+            dt=user_inputs["dt"],
+            igb =user_inputs["igb"],
+            saltcon =user_inputs["saltcon"],
+            rgbmax=user_inputs["rgbmax"],
+            gbsa=user_inputs["gbsa"],
+            temp0=user_inputs["temp0"],
+            ntpr=user_inputs["ntpr"],
+            ntwx=user_inputs["ntwx"],
+            cut=user_inputs["cut"],
+            ntc= user_inputs["ntc"],
             nmropt=1
             )
 
 
     if turn_on_conformational_rest != None and turn_off_solvent == False:
          final_template = template.substitute(
-            nstlim=100,
+            nstlim=user_inputs["nstlim"],
             ntx=1,
             irest=0,
-            dt=0.001,
-            igb = 2,
-            saltcon = 0.3,
-            gbsa=0,
-            temp0=298,
-            ntpr=10,
-            ntwx=10,
-            cut=999,
+            dt=user_inputs["dt"],
+            igb =user_inputs["igb"],
+            saltcon =user_inputs["saltcon"],
+            rgbmax=user_inputs["rgbmax"],
+            gbsa=user_inputs["gbsa"],
+            temp0=user_inputs["temp0"],
+            ntpr=user_inputs["ntpr"],
+            ntwx=user_inputs["ntwx"],
+            cut=user_inputs["cut"],
+            ntc= user_inputs["ntc"],
             nmropt=1
             )
     
     if turn_on_conformational_rest != None and turn_off_solvent:
         final_template = template.substitute(
-            nstlim=100,
+            nstlim=user_inputs["nstlim"],
             ntx=1,
             irest=0,
-            dt=0.001,
+            dt=user_inputs["dt"],
             igb = 6,
             saltcon = 0.0,
-            gbsa=0,
-            temp0=298,
-            ntpr=10,
-            ntwx=10,
-            cut=999,
+            rgbmax=user_inputs["rgbmax"],
+            gbsa=user_inputs["gbsa"],
+            temp0=user_inputs["temp0"],
+            ntpr=user_inputs["ntpr"],
+            ntwx=user_inputs["ntwx"],
+            cut=user_inputs["cut"],
+            ntc= user_inputs["ntc"],
             nmropt=1
             )
     with open('mdin', "w") as output:
