26c016f068a284ebac037f83146d9fba722c1e3c
Steven Ayoub <steven.ayoub.362@my.csun.edu>
2022-03-08 21:12:18 -0800

    created run_intermidate(*args) fn
    setup and parse in all files required to run MD simulations

--- implicit_solvent_ddm/simulations.py
+++ implicit_solvent_ddm/simulations.py
@@ -14,12 +14,82 @@ from string import Template
 #local imports 
 from implicit_solvent_ddm.restraints import write_empty_restraint_file
 from implicit_solvent_ddm.restraints import write_restraint_forces
-from implicit_solvent_ddm.alchemical import turn_off_charges 
+from implicit_solvent_ddm.toil_parser import get_output_dir
 from implicit_solvent_ddm.alchemical import alter_topology_file
 from implicit_solvent_ddm.remd import copy 
 from implicit_solvent_ddm.remd import write_empty_restraint
 from implicit_solvent_ddm.IO import export_outputs
 
+def run_intermidate(job, solute_cordinate, config_args, intermidate_args):
+    
+    # job.log("this is the current job running {}".format(message))
+    # job.log(f"input mdin {input_mdin}")
+    tempDir = job.fileStore.getLocalTempDir()
+    
+    #make a global copy of topology and coordinate file 
+    solute_file = intermidate_args["topology"]
+    solute_filename = os.path.basename(solute_file)
+    solute = job.fileStore.readGlobalFile(solute_file,  userPath=os.path.join(tempDir, os.path.basename(solute_file)))
+    rst = job.fileStore.readGlobalFile(solute_cordinate[0], userPath=os.path.join(tempDir, os.path.basename(solute_cordinate[0])))
+    #read in intermidate mdin 
+    mdin_filename = job.fileStore.importFile("file://" + make_mdin_file(config_args, 
+                                                                        intermidate_args["conformational_restraint"], intermidate_args["charge_off"]))
+    mdin = job.fileStore.readGlobalFile(mdin_filename, userPath=os.path.join(tempDir, os.path.basename(mdin_filename)))
+    
+    if intermidate_args["solvent_turned_off"]:      
+        altered_solute_filename = job.fileStore.importFile("file://" + alter_topology_file(solute, rst, config_args, intermidate_args))
+        #solute = job.fileStore.readGlobalFile(charge_off_solute_filename,  userPath=os.path.join(tempDir, os.path.basename(charge_off_solute_filename)))
+        solute = job.fileStore.readGlobalFile(altered_solute_filename, userPath=os.path.join(tempDir, os.path.basename(altered_solute_filename)))
+    
+    if intermidate_args["conformational_restraint"]:
+        conformational_restraint = str(intermidate_args["conformational_restraint"]) 
+        orientational_restraint = intermidate_args["orientational_restraints"]
+        job.log("CONFORMATIONAL TURN ON")
+        restraint_file = job.fileStore.importFile("file://" + write_restraint_forces(solute_filename, config_args["workDir"], intermidate_args["conformational_restraint"], intermidate_args["orientational_restraints"])) 
+        restraint_basename = os.path.basename(restraint_file)
+        job.log('restraint_freeze_file : ' + str(restraint_file))
+        restraint = job.fileStore.readGlobalFile(restraint_file, userPath=os.path.join(tempDir, restraint_basename))
+        #get output directory 
+        output_dir = get_output_dir(intermidate_args["topology"],intermidate_args["state_label"], config_args["workDir"])
+        job.log(f"output directory {output_dir}")
+        if intermidate_args["orientational_restraints"] != None:
+            conformational_restraint = intermidate_args["conformational_restraint"]
+            orientational_restraint = intermidate_args["orientational_restraints"]            
+            if not os.path.exists(output_dir + '/' + str(conformational_restraint) + '_' + str(orientational_restraint)):
+                output_dir = os.path.join(output_dir + '/'+ str(conformational_restraint) + '_' + str(orientational_restraint))
+                os.makedirs(output_dir)
+        else:
+        #make directory for specific conformational restraint force 
+            if not os.path.exists(f"{output_dir}/{conformational_restraint}"):
+                output_dir = os.path.join(f"{output_dir}/{conformational_restraint}")
+                os.makedirs(output_dir)
+                job.log(f"no orientaional restraints make output dir {output_dir}")
+        
+        
+    files_in_current_directory = os.listdir(tempDir)  
+    job.log(f"files before simulations {files_in_current_directory}")
+    
+    #name restart and trajectory files 
+    solu = re.sub(r"\..*","",solute_filename)
+    message = intermidate_args["message"]
+    restart_filename = f"{message}_{solu}.rst7"
+    trajectory_filename = f"{solu}.nc"
+    
+    run_args = {'solute': solute,
+                'coordinate': rst,
+                "restart_filename": restart_filename,
+                "trajectory_filename": trajectory_filename,
+                "mdin": mdin
+                }
+    submit_job(job, config_args, run_args)
+ 
+    #export files 
+    restrt_file,mdcrd_file = export_outputs(job, output_dir, files_in_current_directory)
+    job.fileStore.exportFile(solute, "file://" + os.path.abspath(os.path.join(output_dir, str(os.path.basename(solute)))))
+    job.fileStore.exportFile(restraint, "file://" + os.path.abspath(os.path.join(output_dir,"restraint.RST")))
+
+    return (restrt_file,mdcrd_file)
+    
 def run_md(job, solute_file, solute_filename, solute_rst, solute_rst_filename, output_dir, argSet, message, COM=False, input_mdin=None, work_dir= None, ligand_mask = None, receptor_mask = None, conformational_restraint = None, orientational_restraint = None, solvent_turned_off=False, charge_off = False, exculsions=False):
     """
     Locally run AMBER library engines for molecular dynamics
@@ -229,9 +299,7 @@ def make_mdin_file(arguments, turn_on_conformational_rest, turn_off_solvent):
     mdin: str
         Absolute path where the MD input file was created.
     """
-    with open(arguments["parameters"]["mdin_intermidate_config"]) as mdin_values:
-        user_inputs = yaml.safe_load(mdin_values)
-    
+    user_inputs = arguments["parameters"]["mdin_intermidate_config"]
     mdin_path = os.path.abspath(os.path.dirname(
                 os.path.realpath(__file__)) + "/templates/mdgb.mdin")
     
