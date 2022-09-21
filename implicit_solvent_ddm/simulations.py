624d0cdbabb97bea58b7de3ce2fe4c3b894f877d
Steven Ayoub <steven.ayoub.362@my.csun.edu>
2022-09-20 20:28:16 -0700

    system_type

--- implicit_solvent_ddm/simulations.py
+++ implicit_solvent_ddm/simulations.py
@@ -226,6 +226,7 @@ class Simulation(Calculation):
         input_file,
         restraint_file: Union[RestraintMaker, str],
         directory_args,
+        system_type: Optional[str]=None, 
         conformational_force=None, 
         orientational_force=None, 
         dirstruct="dirstruct",
@@ -254,7 +255,7 @@ class Simulation(Calculation):
             inptraj=inptraj,
         )
         self.restraint_key = restraint_key 
-    
+        self.system_type = system_type
     def setup(self):
         """
         Sets up the command-line arguments. Sander requires a unique restrt file
