a92fb5f28b73a685c945c6fb49a76e87502c8aa4
Steven Ayoub <steven.ayoub.362@my.csun.edu>
2022-09-27 14:31:49 -0700

    refactored main.py
    purpose of refactor is to remove the recursive method to create post processing simulations
    
    The main() function just setups simulations, creating the orientational and conformational restraint
    
    Only simulations that will be run  in main() are the endstate simulations and every intermidate state simulation will be handle through the runner.py class

--- implicit_solvent_ddm/run_implicit_ddm.py
+++ implicit_solvent_ddm/run_implicit_ddm.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
-import implicit_solvent_ddm.implicit_ddm_workflow as implicit_ddm_workflow
+#import implicit_solvent_ddm.implicit_ddm_workflow as implicit_ddm_workflow
+import implicit_solvent_ddm.refactor_main as refactor_main
 
 if __name__ == '__main__':
-    ex = implicit_ddm_workflow.main()
+    ex = refactor_main.main()
 
 
