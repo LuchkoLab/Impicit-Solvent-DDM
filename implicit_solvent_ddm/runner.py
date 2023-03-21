043559f53433f62f2d6032d47c6995dff325a09e
Steven Ayoub <steven.ayoub.362@my.csun.edu>
2023-03-21 13:37:12 -0700

    post analysis only
    if flag -post_only is given will read in already completed parsed dataframes

--- implicit_solvent_ddm/runner.py
+++ implicit_solvent_ddm/runner.py
@@ -1,7 +1,5 @@
-
-import itertools
-import logging
 import os
+import re
 from email import message
 from importlib.metadata import files
 from pathlib import Path
@@ -21,111 +19,318 @@ from implicit_solvent_ddm.simulations import Simulation
 
 
 class IntermidateRunner(Job):
-    #simulations: dict[Simulation, int]
-    def __init__(self, 
-                 simulations: list[Simulation], restraints:RestraintMaker, 
-                 post_process_no_solv_mdin: FileID, post_process_mdin: FileID,
-                 post_process_distruct: str, memory: Optional[Union[int, str]] = None, cores: Optional[Union[int, float, str]] = None, disk: Optional[Union[int, str]] = None, preemptable: Optional[Union[bool, int, str]] = None, unitName: Optional[str] = "", checkpoint: Optional[bool] = False, displayName: Optional[str] = "", descriptionClass: Optional[str] = None) -> None:
-       
-        super().__init__(memory, cores, disk, preemptable, unitName, checkpoint, displayName, descriptionClass)
-      
+    # simulations: dict[Simulation, int]
+    def __init__(
+        self,
+        simulations: list[Simulation],
+        restraints: RestraintMaker,
+        post_process_no_solv_mdin: FileID,
+        post_process_mdin: FileID,
+        post_process_distruct: str,
+        post_only: bool,
+        memory: Optional[Union[int, str]] = None,
+        cores: Optional[Union[int, float, str]] = None,
+        disk: Optional[Union[int, str]] = None,
+        preemptable: Optional[Union[bool, int, str]] = None,
+        unitName: Optional[str] = "",
+        checkpoint: Optional[bool] = False,
+        displayName: Optional[str] = "",
+        descriptionClass: Optional[str] = None,
+    ) -> None:
+
+        super().__init__(
+            memory,
+            cores,
+            disk,
+            preemptable,
+            unitName,
+            checkpoint,
+            displayName,
+            descriptionClass,
+        )
+
         self.simulations = simulations
         self.restraints = restraints
         self.no_solvent_mdin = post_process_no_solv_mdin
         self.mdin = post_process_mdin
+        self.post_only = post_only
         self.post_output = []
         self.ligand_output = []
         self.receptor_output = []
-        self.complex_output = [] 
+        self.complex_output = []
         self.post_process_distruct = post_process_distruct
-  
-    
+
     def run(self, fileStore):
-        
-        def run_post_process(job:Job, ran_simulation:Simulation):
-            
+        fileStore.logToMaster(f"post only is {self.post_only}")
+
+        def run_post_process(job: Job, ran_simulation: Simulation):
+
             for post_simulation in self.simulations:
+
+                directory_args = post_simulation.directory_args.copy()
+
+                directory_args.update(self.update_postprocess_dirstruct(ran_simulation.directory_args))  # type: ignore
+                #fileStore.logToMaster(f"RUNNER directory args {directory_args}\n")
+
+                mdin = self.mdin
                 
-                #check if the system are the same. ligand with ligand | receptor with receptor ect 
-                if ran_simulation.system_type == post_simulation.system_type:
+                if post_simulation.directory_args["igb_value"] == 6:
+                    mdin = self.no_solvent_mdin
+
+                # run simulation if its not endstate with endstate
+                #if post_simulation.post: 
+                if (
+                    post_simulation.inptraj != ran_simulation.inptraj
+                    or post_simulation.inptraj == None
+                ):
+                    # endstate simulation has inptraj attribute 
+                    if ran_simulation.inptraj is not None:
+                        input_traj = ran_simulation.inptraj
                     
-                    directory_args = post_simulation.directory_args.copy()
-        
-                    directory_args.update(self.update_postprocess_dirstruct(ran_simulation.directory_args, job.rv(1)))  # type: ignore
-                    fileStore.logToMaster(f"RUNNER directory args {directory_args}\n")
-                    
-                    mdin = self.mdin
-                    if post_simulation.directory_args["igb_value"] == 6:
-                       mdin = self.no_solvent_mdin
-                        
-                    #run simulation if its not endstate with endstate 
-                    if post_simulation.inptraj != ran_simulation.inptraj or post_simulation.inptraj == None: 
-                        post_dirstruct = self.get_system_dirs(post_simulation.system_type)
-                        fileStore.logToMaster(f"get_system_dirs {post_simulation.system_type}")
-                        fileStore.logToMaster(f"current dirstruct {post_dirstruct}")
-                        fileStore.logToMaster(f"inptraj ERROR {job.rv(1)}")
-                        post_process_job = Simulation(executable=post_simulation.executable, 
-                                                    mpi_command=post_simulation.mpi_command, 
-                                                    num_cores=post_simulation.num_cores, 
-                                                    prmtop=post_simulation.prmtop, incrd=post_simulation.incrd,
-                                                    input_file=mdin, restraint_file=post_simulation.restraint_file, 
-                                                    working_directory=post_simulation.working_directory,
-                                                    directory_args=directory_args, dirstruct=post_dirstruct, 
-                                                    inptraj=job.rv(1), restraint_key=post_simulation.restraint_key)
-                        job.addChild(post_process_job)
-                        data_frame = post_process_job.addFollowOnJobFn(create_mdout_dataframe, post_process_job.directory_args, post_process_job.dirstruct, post_process_job.output_dir)
-                    
+                    # whereas the intermidate states does not 
                     else:
-                        data_frame = job.addFollowOnJobFn(create_mdout_dataframe, directory_args, post_simulation.dirstruct, post_simulation.output_dir)
-                
-                    self.post_output.append(data_frame.rv())
-                    #self.system_specific_add(post_simulation.system_type, data_frame=data_frame.rv())
-        
-        # iterate and submit all intermidate simulations. Then followup with post-process        
+                        input_traj = job.rv(1)
+                    #if job -> endstate 
+                    #if post is endstate wont access job.rv()
+                    #but if post is not endstate then it will aceess job.rv
+
+                    post_dirstruct = self.get_system_dirs(post_simulation.system_type)
+                    fileStore.logToMaster(
+                        f"get_system_dirs {post_simulation.system_type}"
+                    )
+                    fileStore.logToMaster(f"current dirstruct {post_dirstruct}")
+                    post_process_job = Simulation(
+                        executable="sander.MPI",
+                        mpi_command=post_simulation.mpi_command,
+                        num_cores=post_simulation.num_cores,
+                        prmtop=post_simulation.prmtop,
+                        incrd=post_simulation.incrd,
+                        input_file=mdin,
+                        restraint_file=post_simulation.restraint_file,
+                        working_directory=post_simulation.working_directory,
+                        directory_args=directory_args,
+                        dirstruct=post_dirstruct,
+                        inptraj=input_traj,
+                        post_analysis=True,
+                        restraint_key=post_simulation.restraint_key,
+                    )
+                    job.addChild(post_process_job)
+
+                    data_frame = post_process_job.addFollowOnJobFn(
+                        create_mdout_dataframe,
+                        post_process_job.directory_args,
+                        post_process_job.dirstruct,
+                        post_process_job.output_dir,
+                    )
+
+                else:
+                    fileStore.logToMaster(f"parsing endstate post only")
+                    data_frame = job.addFollowOnJobFn(
+                        create_mdout_dataframe,
+                        directory_args,
+                        post_simulation.dirstruct,
+                        post_simulation.output_dir,
+                    )
+
+                self.post_output.append(data_frame.rv())
+
+        # iterate and submit all intermidate simulations. Then followup with post-process
         for simulation in self.simulations:
-            run_post_process(job=self.addChild(simulation), ran_simulation = simulation)
-            
+            # only post analysis
+           
+            if self.post_only:
+                
+                if self._check_mdout(simulation=simulation) or simulation.inptraj != None:
+                    fileStore.logToMaster("simulation mdout may exisit?")
+                    
+                    if simulation.inptraj == None:
+                        fileStore.logToMaster(
+                            f"get mdtraj at directory:\n {simulation.output_dir}"
+                            )
+                        simulation.inptraj = [
+                            fileStore.import_file(
+                                "file://" + self._get_md_traj(simulation, fileStore),
+                            )
+                        ]
+                    self.only_post_analysis(
+                        simulation, md_traj=simulation.inptraj, fileStore=fileStore
+                    )
+                else:
+                    fileStore.logToMaster(f"RUNNING MD THEN POST")
+                    fileStore.logToMaster(f"mdout does not exist path: {simulation.output_dir}")
+                    run_post_process(
+                        job=self.addChild(simulation), ran_simulation=simulation
+                    )
+                    
+            # if self.post_only:
+            #     if simulation.inptraj is None:
+
+            #         fileStore.logToMaster(
+            #             f"get mdtraj at directory:\n {simulation.output_dir}"
+            #         )
+            #         simulation.inptraj = [
+            #             fileStore.import_file(
+            #                 "file://" + self._get_md_traj(simulation, fileStore),
+            #             )
+            #         ]
+
+            #     self.only_post_analysis(
+            #         simulation, md_traj=simulation.inptraj, fileStore=fileStore
+            #     )
+            else:
+                fileStore.logToMaster("DRY RUNNN")
+                run_post_process(
+                    job=self.addChild(simulation), ran_simulation=simulation
+                )
+
         return self.post_output
-    
+
+    def only_post_analysis(self, completed_sim: Simulation, md_traj, fileStore):
+        """Assumes MD has already been completed and will only run post-analysis."""
+        for post_simulation in self.simulations:
+
+            directory_args = post_simulation.directory_args.copy()
+            # fileStore.logToMaster(f"args {completed_sim.directory_args} & {md_traj}")
+            directory_args.update(self.update_postprocess_dirstruct(completed_sim.directory_args))  # type: ignore
+
+            mdin = self.mdin
+            if post_simulation.directory_args["igb_value"] == 6:
+                mdin = self.no_solvent_mdin
+
+            # run simulation if its not endstate with endstate
+            post_dirstruct = self.get_system_dirs(post_simulation.system_type)
+
+            post_process_job = Simulation(
+                executable="sander.MPI",
+                mpi_command=post_simulation.mpi_command,
+                num_cores=post_simulation.num_cores,
+                prmtop=post_simulation.prmtop,
+                incrd=post_simulation.incrd,
+                input_file=mdin,
+                restraint_file=post_simulation.restraint_file,
+                working_directory=post_simulation.working_directory,
+                directory_args=directory_args,
+                dirstruct=post_dirstruct,
+                inptraj=md_traj,
+                post_analysis=True,
+                restraint_key=post_simulation.restraint_key,
+            )
+            if not "simulation_mdout.parquet.gzip" in os.listdir(post_process_job.output_dir):
+                fileStore.logToMaster(
+                    f"RUNNING post analysis with inptraj trajecory: {md_traj}"
+                )
+                # fileStore.logToMaster(
+                #     f"output postProces: {post_process_job.output_dir}"
+                # )
+                fileStore.logToMaster(
+                    f"State potential energy {post_simulation.directory_args['state_label']}"
+                )
+                # fileStore.logToMaster(f"state args: {post_simulation.directory_args}")
+
+                self.addChild(post_process_job)
+
+                data_frame = post_process_job.addFollowOnJobFn(
+                    create_mdout_dataframe,
+                    post_process_job.directory_args,
+                    post_process_job.dirstruct,
+                    post_process_job.output_dir,
+                )
+
+                self.post_output.append(data_frame.rv())
+            else:
+                fileStore.logToMaster("Retrieving completed post-analysis data")
+                self.post_output.append(
+                    pd.read_parquet(
+                        os.path.join(post_process_job.output_dir, "simulation_mdout.parquet.gzip"), 
+                    )
+                )
+
     @staticmethod
-    def update_postprocess_dirstruct(run_time_args:dict, inptaj)->dict[str, Union[str, object]]:
+    def _get_md_traj(simulation: Simulation, fileStore):
+
+        try:
+            return os.path.join(
+                simulation.output_dir,
+                list(
+                    filter(
+                        lambda file: re.match(r"^.*\.nc$", file),
+                        os.listdir(simulation.output_dir),
+                    )
+                )[0],
+            )
+        except:
+            fileStore.logToMaster(f"except clause: ")
+            fileStore.logToMaster(f"old output dir {simulation.output_dir}")
+
+            if re.search(r"-0.2857142857142864", simulation.output_dir):
+                simulation.output_dir = re.sub(
+                    r"-0.2857142857142864",
+                    "-0.2857142857142865",
+                    simulation.output_dir,
+                )
+            else:
+                simulation.output_dir = re.sub(
+                    r"-0.2857142857142865",
+                    "-0.2857142857142864",
+                    simulation.output_dir,
+                )
+
+        fileStore.logToMaster(f"new output dir {simulation.output_dir}")
+
+        return os.path.join(
+            simulation.output_dir,
+            list(
+                filter(
+                    lambda file: re.match(r"^.*\.nc$", file),
+                    os.listdir(simulation.output_dir),
+                )
+            )[0],
+        )
+
+    @staticmethod
+    def _check_mdout(simulation: Simulation) -> bool:
         
+        if "mdout" in  os.listdir(simulation.output_dir):
+            for line in reversed(open(os.path.join(simulation.output_dir, "mdout")).readlines()):
+                if "Final Performance Info" in line:
+                    return True        
+        return False 
+                
+        
+    @staticmethod
+    def update_postprocess_dirstruct(
+        run_time_args: dict
+    ) -> dict[str, Union[str, object]]:
+
         if "orientational_restraints" in run_time_args.keys():
             return {
                 "traj_state_label": run_time_args["state_label"],
-                "trajectory_restraint_conrest": run_time_args["conformational_restraint"],
-                "trajectory_restraint_orenrest": run_time_args["orientational_restraints"],
-                "traj_igb": run_time_args["igb"],
-                "traj_charge": run_time_args["charge"], 
-                "filename": f"{run_time_args['filename']}_postprocess",
-                "runtype": f"Running post process with trajectory: {inptaj}",       
-            }
-        else:
-            return {
-                "traj_state_label": run_time_args["state_label"],
-                "trajectory_restraint_conrest": run_time_args["conformational_restraint"],
+                "trajectory_restraint_conrest": run_time_args[
+                    "conformational_restraint"
+                ],
+                "trajectory_restraint_orenrest": run_time_args[
+                    "orientational_restraints"
+                ],
+                "traj_extdiel": run_time_args["extdiel"],
                 "traj_igb": run_time_args["igb"],
                 "traj_charge": run_time_args["charge"],
                 "filename": f"{run_time_args['filename']}_postprocess",
-                "runtype": f"Running post process with trajectory: {inptaj}",
-                
             }
+
+        return {
+            "traj_state_label": run_time_args["state_label"],
+            "trajectory_restraint_conrest": run_time_args["conformational_restraint"],
+            "traj_igb": run_time_args["igb"],
+            "traj_extdiel": run_time_args["extdiel"],
+            "traj_charge": run_time_args["charge"],
+            "filename": f"{run_time_args['filename']}_postprocess",
+        }
+
     @staticmethod
     def get_system_dirs(system_type):
         if system_type == "ligand" or system_type == "receptor":
             return "post_process_apo"
-        
-        return "post_process_halo"
-    
-    
-    def system_specific_add(self, system_type, data_frame):
-        
-        if system_type == 'ligand': 
-            self.ligand_output.append(data_frame)
-                    
-        elif system_type == 'receptor':
-            self.receptor_output.append(data_frame)
-        
-        else: 
-            self.complex_output.append(data_frame)
 
+        return "post_process_halo"
+
+    # /nas0/ayoub/sampl9_runs/sampl9_extend_windows_diel/WP6_G2_Hmass/lambda_window/1.0/78.5/-0.2857142857142864/3.7142857142857135/WP6_G2_Hmass_state_8_0.8203353560076375_13.1253656961222_prod_traj.nc
