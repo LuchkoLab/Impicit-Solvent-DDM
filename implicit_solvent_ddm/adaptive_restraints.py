c163a4975654b0fb690bc807b3427fe0bfe1954d
Steven Ayoub <steven.ayoub.362@my.csun.edu>
2023-06-03 21:10:48 -0700

    Iterative processs for improving space phase overlap between neighbors

--- implicit_solvent_ddm/adaptive_restraints.py
+++ implicit_solvent_ddm/adaptive_restraints.py
@@ -12,7 +12,8 @@ AVAGADRO = 6.0221367e23
 BOLTZMAN = 1.380658e-23
 JOULES_PER_KCAL = 4184
 
-def compute_mbar(simulation_data: list[pd.DataFrame], temperature:float, matrix_order:CycleSteps, system:str):
+def compute_mbar(simulation_data: list[pd.DataFrame], temperature:float, matrix_order:CycleSteps, system:str,
+                  memory="2G", cores=1, disk="3G"):
     
     
     def create_mbar_format():
@@ -52,7 +53,7 @@ def compute_mbar(simulation_data: list[pd.DataFrame], temperature:float, matrix_
 
         # divide by Kcal per Kt
         #kcals_per_Kt = ((BOLTZMAN * (AVAGADRO)) / JOULES_PER_KCAL) * temperature
-        
+        print(f"Created Unique MBAR dataframe {df.index.unique()}\n")
         return (df / kcals_per_Kt)
     
     kcals_per_Kt = ((BOLTZMAN * (AVAGADRO)) / JOULES_PER_KCAL) * temperature
@@ -69,7 +70,8 @@ def compute_mbar(simulation_data: list[pd.DataFrame], temperature:float, matrix_
         
     else:
         df_mbar = df_mbar[matrix_order.receptor_order]
-        
+    
+    
     equil_info = pdmbar.detectEquilibration(df_mbar)
 
     df_subsampled = pdmbar.subsampleCorrelatedData(df_mbar, equil_info=equil_info)
@@ -94,25 +96,32 @@ def adpative(job, complex_runner: IntermidateRunner,ligand_runner:IntermidateRun
     cycle_steps.round(3)
     job.log(f"The start matrix: {cycle_steps.start_restraint_matrix}\n")
     job.log(f"complex steps: \n {cycle_steps.complex_order}")
-    results, df = compute_mbar(simulation_data=complex_runner.post_output, 
+    results = compute_mbar(simulation_data=complex_runner.post_output, 
                                temperature=config.intermidate_args.temperature, matrix_order=cycle_steps, 
                                system="complex")
 
-    average = overlap_average(results[-1].computeOverlap()["matrix"], cycle_steps.start_restraint_matrix)
+    average = overlap_average(results[0][-1].computeOverlap()["matrix"], cycle_steps.start_restraint_matrix)
     job.log(f"Average from overlap {average}")
     
-    job.addChildJobFn(improve_overlap, complex_runner, ligand_runner, receptor_runner, average, config).rv()
-    # if good_enough(average=average):
-    #     job.log(f"THE OVERLAP IS ABOVE 0.03")
-    #     ligand_mbar = job.wrapFn(compute_mbar, ligand_runner.post_only, config.intermidate_args.temperature)
-    #     receptor_mbar = job.wrapFn(compute_mbar, receptor_runner.post_only, config.intermidate_args.temperature)
-        
-    #     return results, job.addChildJobFn(receptor_mbar), job.addChildJobFn(ligand_mbar)
+    #job.addChildJobFn(improve_overlap, complex_runner, ligand_runner, receptor_runner, average, config).rv()
     
-    # else:
-    #     job.log(f"POOR OVERLAP BEGING adding windows")
-    #     #improve the method 
-    #     job.addFollowonJobFn(adpative, job.addChildJobFn(improve_overlap, complex_runner, ligand_runner, receptor_runner, average).rv())
+     
+    if good_enough(average=average):
+        job.log(f"THE OVERLAP IS ABOVE 0.03")
+        ligand_mbar = job.wrapFn(compute_mbar, simulation_data=ligand_runner.post_output, temperature=config.intermidate_args.temperature, 
+                                 matrix_order=cycle_steps, system="ligand"
+                                )
+        receptor_mbar = job.wrapFn(compute_mbar, receptor_runner.post_output, config.intermidate_args.temperature,
+                                   matrix_order=cycle_steps, system="receptor"
+                                )
+        
+        return results, job.addChild(ligand_mbar).rv(), job.addChild(receptor_mbar).rv(), config
+    
+    else:
+        job.log(f"POOR OVERLAP BEGING adding windows")
+        #improve the method 
+        improve_job = job.addChildJobFn(improve_overlap, complex_runner, ligand_runner, receptor_runner, average, config)
+        job.addFollowOnJobFn(adpative, improve_job.rv(0), improve_job.rv(1), improve_job.rv(2), improve_job.rv(3))
 
 
 
@@ -138,7 +147,7 @@ def group_overlap_neighbors(matrix):
     size = matrix.shape[0] - 1
     
     
-    def newrecur(n=0, new=[]):
+    def get_overlap_neighbors(n=0, new=[]):
     
         if n == size:
             return new 
@@ -149,51 +158,69 @@ def group_overlap_neighbors(matrix):
             new.append((a,b))
 
             
-            return newrecur(n+1 ,new=new)
+            return get_overlap_neighbors(n+1 ,new=new)
     
-    return newrecur()
+    return get_overlap_neighbors()
 
-def improve_overlap(job, comp:IntermidateRunner, ligand:IntermidateRunner, receptor:IntermidateRunner, avg_overlap, config):
+def improve_overlap(job, comp:IntermidateRunner, ligand:IntermidateRunner, receptor:IntermidateRunner, avg_overlap, config:Config):
 
-    conformational = config.intermidate_args.exponent_conformational_forces
-    orient = config.intermidate_args.exponent_orientational_forces
+    conformational = config.intermidate_args.exponent_conformational_forces.copy()
+    orient = config.intermidate_args.exponent_orientational_forces.copy()
 
+    restraints_job = job.addChildJobFn(initilized_jobs)
+    job.log(f"Conformational windows: {conformational}\n")
+    job.log(f"Orientational windows: {orient}\n")
     for index, a in enumerate(avg_overlap):
+        con_window = conformational[index] - 1
+        orient_window = orient[index] - 1
+        
         if a < 0.03:
+            
             #config.lower_bound != None
-            if False:
+            if con_window in config.intermidate_args.exponent_conformational_forces:
+                job.log(f"INDEX VALUE {index}")
+                job.log(f"Divide: {con_window} + {conformational[index]}")
                 #bisect 
-                pass
-            else:
-                job.log(f"ADD WINDOWS\n")
-                new_con = np.exp2(conformational[index] - 1)
-                new_orient = np.exp2(orient[index] - 1)
-                job.log(f"Conformational window: {new_con}\n")
-                job.log(f"Orientational window: {new_orient}\n")
-                restraints_job = job.addChildJobFn(initilized_jobs)
-                comp._add_complex_simulation(conformational=conformational[index] - 1, orientational=orient[index] - 1, 
-                                                 mdin=config.inputs["default_mdin"], restraint_file=restraints_job.addChildJobFn(write_restraint_forces, 
-                                                                                                       conformational_template=comp.restraints.complex_conformational_restraints,
-                                                                                                       orientational_template=comp.restraints.boresch.boresch_template,
-                                                                                                       conformational_force=new_con,
-                                                                                                       orientational_force=new_orient))
-                ligand._add_ligand_simulation(conformational=new_con, mdin=config.inputs["default_mdin"], restraint_file = restraints_job.addChildJobFn(write_restraint_forces,
-                                                                                                                                  conformational_template=ligand.restraints.ligand_conformational_restraints,
-                                                                                                                                  conformational_force=new_con))
-                receptor._add_receptor_simulation(conformational=new_con, mdin=config.inputs["default_mdin"], restraint_file = restraints_job.addChildJobFn(write_restraint_forces,
-                                                                                                                                  conformational_template=ligand.restraints.receptor_conformational_restraints,
-                                                                                                                                  conformational_force=new_con))
-                config.intermidate_args.exponent_conformational_forces.append(conformational[index] - 1)
-                config.intermidate_args.exponent_orientational_forces.append(orient[index] - 1)
+                con_window = (con_window +  conformational[index])/2
+                orient_window = (orient_window + orient[index])/2
                 
-                restraints_done = restraints_job.addFollowOnJobFn(initilized_jobs)    
+            job.log(f"ADD WINDOWS\n")
+            new_con = np.exp2(con_window)
+            new_orient = np.exp2(orient_window)
+            job.log(f"Conformational window: {con_window}. np.exp2({con_window}): {new_con}\n")
+            job.log(f"Orientational window: {orient_window}\n")
+            comp._add_complex_simulation(conformational=con_window, orientational=orient_window, 
+                                                mdin=config.inputs["default_mdin"], restraint_file=restraints_job.addChildJobFn(write_restraint_forces, 
+                                                                                                    conformational_template=comp.restraints.complex_conformational_restraints,
+                                                                                                    orientational_template=comp.restraints.boresch.boresch_template,
+                                                                                                    conformational_force=new_con,
+                                                                                                    orientational_force=new_orient))
+            ligand._add_ligand_simulation(conformational=con_window, mdin=config.inputs["default_mdin"], restraint_file = restraints_job.addChildJobFn(write_restraint_forces,
+                                                                                                                                conformational_template=ligand.restraints.ligand_conformational_restraints,
+                                                                                                                                conformational_force=new_con))
+            receptor._add_receptor_simulation(conformational=con_window, mdin=config.inputs["default_mdin"], restraint_file = restraints_job.addChildJobFn(write_restraint_forces,
+                                                                                                                                conformational_template=ligand.restraints.receptor_conformational_restraints,
+                                                                                                                                conformational_force=new_con))
+            config.intermidate_args.exponent_conformational_forces.append(con_window)
+            config.intermidate_args.exponent_orientational_forces.append(orient_window)
+    
+    config.intermidate_args.exponent_conformational_forces.sort(reverse=True)
+    config.intermidate_args.exponent_orientational_forces.sort(reverse=True)
+            
+    restraints_done = restraints_job.addFollowOnJobFn(initilized_jobs)    
     job.log(f"RUNNER with new restraints window")
-    new_com = receptor.new_runner(config, comp.__dict__)
-    job.log(f"new simulation args for restriants RECEPTOR {new_com.simulations[-1].directory_args}")
-    restraints_done.addChild(new_com.simulations[-1])
-    #restraints_done.addChild(comp.new_runner(config, comp.__dict__))
-    #return (restraints_done.addChild(comp.new_runner(config, comp.__dict__)), restraints_done.addChild(ligand.new_runner(config, ligand.__dict__)), restraints_done.addChild(receptor.new_runner(config, receptor.__dict__)), config)
-
+    # new_com = comp.new_runner(config, comp.__dict__)
+    # job.log(f"new simulation args for restriants RECEPTOR {new_com.simulations[-1].directory_args}")
+    # restraints_done.addChild(new_com)
+    # restraints_done.addChild(comp.new_runner(config, comp.__dict__))
+    #restraints_done.addChild(ligand.new_runner(config, ligand.__dict__))
+    #restraints_done.addChild(receptor.new_runner(config, receptor.__dict__))
+    
+    return (restraints_done.addChild(comp.new_runner(config, comp.__dict__)).rv(), 
+            restraints_done.addChild(ligand.new_runner(config, ligand.__dict__)).rv(), 
+            restraints_done.addChild(receptor.new_runner(config, receptor.__dict__)).rv(), 
+            config
+        )
 
 
 def initilized_jobs(job):
